1dcecf676d013d4ed480d28b54c9db9f16c77150
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-09-25 17:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n=s.length;
+	
+       let freq = new Array(256).fill(0);
+
+	   for(let i=0;i<n;i++){
+	       freq[s[i].charCodeAt(0)]++;
+	   }
+	for(let i=0;i<n;i++){
+      if(freq[s[i].charCodeAt(0)] > 0 &&  freq[s[i].charCodeAt(0)] == 1){
+	     return s.charAt(i);
+	  }
+	}
+ return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
